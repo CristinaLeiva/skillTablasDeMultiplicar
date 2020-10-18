035b29e99c4193a130509cd10a37fb870b26dd58
CristinaLeiva <72863416+CristinaLeiva@users.noreply.github.com>
2020-10-18 21:01:29 +0200

    Add files via upload
    Primera versión funcional de la skill.
    
    NIVEL BÁSICO: alexa pide al usuario una tabla y le pregunta aleatoriamente. Si acierta le felicita y si falla le da el resultado y le pregunta otra.

---
+++ lambda/index.js
@@ -0,0 +1,242 @@
+//Esta skill ayuda a aprender las tablas de multiplicar
+//   NIVEL BÁSICO: Alexa pregunta por orden la tabla de multiplicar que el usuario le indica. Si acierta --> Felicita. Sino --> Devuelve resultado y anima a continuar.
+//   NIVEL AVANZADO: Alexa pregunta aleatoriamente la tabla de multiplicar que el usuario le indica. Si acierta --> Felicita. Sino --> Devuelve resultado y anima a continuar.
+//   NIVEL EXPERTO: Alexa pregunta aleatoriamente cualquier tabla de multiplicar. Si acierta --> Felicita. Sino --> Devuelve resultado y anima a continuar.
+//   OTRAS OPCIONES DE DESARROLLO:
+//      - Qué el usuario diga una tabla y Alexa le diga si es correcta.
+//      - Sistema de puntos para motivar.
+//      - Multiplicaciones de dos cifras por una cifra.
+//Permisos necesarios:
+//  Acceso al nombre de usuario del dispositivo únicamente para hacer más personal la skill, nunca se utilizará para otros fines ni para cederla a terceros.
+
+const Alexa = require('ask-sdk-core');
+const persistence = require('./persistence');
+const interceptors = require('./interceptors');
+const logic = require('./logic');
+const constants = require('./constants');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+        const {attributesManager, serviceClientFactory, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        if(!sessionAttributes['name']){
+            // let's try to get the given name via the Customer Profile API
+            // don't forget to enable this permission in your skill configuratiuon (Build tab -> Permissions)
+            // or you'll get a SessionEndedRequest with an ERROR of type INVALID_RESPONSE
+            try {
+                const {permissions} = requestEnvelope.context.System.user;
+                if(!permissions)
+                    throw { statusCode: 401, message: 'No permissions available' }; // there are zero permissions, no point in intializing the API
+                const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+                const profileName = await upsServiceClient.getProfileGivenName();
+                if (profileName) { // the user might not have set the name
+                  //save to session and persisten attributes
+                  sessionAttributes['name'] = profileName;
+                }
+
+            } catch (error) {
+                console.log(JSON.stringify(error));
+                if (error.statusCode === 401 || error.statusCode === 403) {
+                  // the user needs to enable the permissions for given name, let's send a silent permissions card.
+                  handlerInput.responseBuilder.withAskForPermissionsConsentCard(constants.GIVEN_NAME_PERMISSION);
+                }
+            }
+        }
+
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] + '. ' : '';
+
+        let speechText = handlerInput.t('WELCOME_MSG', {name: name});
+        
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+//el usuario dice "la tabla del 3" o "quiero practicar la tabla del 3"
+const PreguntaLaTablaIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'PreguntaLaTablaIntent';
+    },
+    handle(handlerInput) {
+        const {attributesManager, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const {intent} = requestEnvelope.request;
+        
+        const num_tabla = intent.slots.num_tabla.value;
+        const factor1 = num_tabla;
+        const factor2 = (Math.floor(Math.random() * (10 - 0)) + 0);
+        
+        sessionAttributes['factor1'] = factor1;
+        sessionAttributes['factor2'] = factor2;
+        
+        const speechText = handlerInput.t('PREGUNTA_LA_TABLA_MSG', {num_tabla: num_tabla});
+
+        return handlerInput.responseBuilder
+            .speak(speechText + handlerInput.t('LINEA_DE_TABLA_MSG', {factor1: factor1, factor2: factor2}))
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+//el usuario dice la respuesta a la pregunta, por ejemplo "9"
+const RespuestaUsuarioIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'RespuestaUsuarioIntent';
+    },
+    async handle(handlerInput) {
+        const {attributesManager, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const {intent} = requestEnvelope.request;
+        
+        let factor1 = sessionAttributes['factor1'];
+        let factor2 = sessionAttributes['factor2'];
+        const respuesta_correcta = factor1 * factor2;
+        const respuesta_usuario = intent.slots.respuesta_usuario.value;
+        
+        let speechText = handlerInput.t('RESPUESTA_INCORRECTA_MSG', {respuesta_correcta: respuesta_correcta});
+        if (parseInt(respuesta_correcta) === parseInt(respuesta_usuario)){
+            speechText = handlerInput.t('RESPUESTA_CORRECTA_MSG');
+        }
+        
+        //preparo la siguiente pregunta
+        const factor2_next = (Math.floor(Math.random() * (10 - 0)) + 0);
+        sessionAttributes['factor2'] = factor2_next;
+
+        return handlerInput.responseBuilder
+            .speak(speechText + handlerInput.t('LINEA_DE_TABLA_MSG', {factor1: factor1, factor2: factor2_next}))
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+
+    }
+};
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('HELP_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const name = sessionAttributes['name'] ? sessionAttributes['name'] : '';
+
+        const speechText = handlerInput.t('GOODBYE_MSG', {name: name});
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+    }
+};
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speechText = handlerInput.t('FALLBACK_MSG');
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speechText = handlerInput.t('REFLECTOR_MSG', {intent: intentName});
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speechText = handlerInput.t('ERROR_MSG');
+
+        console.log(`~~~~ Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(handlerInput.t('HELP_MSG'))
+            .getResponse();
+    }
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        PreguntaLaTablaIntentHandler,
+        RespuestaUsuarioIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .addRequestInterceptors(
+        interceptors.LocalisationRequestInterceptor,
+        interceptors.LoggingRequestInterceptor,
+        interceptors.LoadAttributesRequestInterceptor)
+    .addResponseInterceptors(
+        interceptors.LoggingResponseInterceptor,
+        interceptors.SaveAttributesResponseInterceptor)
+    .withPersistenceAdapter(persistence.getPersistenceAdapter())
+    .withApiClient(new Alexa.DefaultApiClient())
+    .lambda();
---
+++ lambda/logic.js
@@ -0,0 +1,115 @@
+const moment = require('moment-timezone'); // will help us do all the birthday math
+const fetch = require('node-fetch');
+
+module.exports = {
+    getAdjustedDateData(timezone) {
+        const today = moment().tz(timezone).startOf('day');
+
+        return {
+            day: today.date(),
+            month: today.month() + 1
+        }
+    },
+    
+    getBirthdayData(day, month, year, timezone) {
+        //timezone = timezone ? timezone : 'Europe/Paris'; // so it works on the simulator, replace with your timezone and remove if testing on a real device
+        const today = moment().tz(timezone).startOf('day');
+        const wasBorn = moment(`${month}/${day}/${year}`, "MM/DD/YYYY").tz(timezone).startOf('day');
+        const nextBirthday = moment(`${month}/${day}/${today.year()}`, "MM/DD/YYYY").tz(timezone).startOf('day');
+        if(today.isAfter(nextBirthday))
+            nextBirthday.add('years', 1);
+        const age = today.diff(wasBorn, 'years');
+        const daysAlive = today.diff(wasBorn, 'days');
+        const daysUntilBirthday = nextBirthday.startOf('day').diff(today, 'days'); // same days returns 0
+
+        return {
+            daysAlive: daysAlive,
+            daysUntilBirthday: daysUntilBirthday,
+            age: age //in years
+        }
+    },
+    
+    createReminderData(daysLeft, timezone, locale, message) {
+        //timezone = timezone ? timezone : 'Europe/Paris'; // so it works on the simulator, replace with your timezone and remove if testing on a real device
+        moment.locale(locale);
+        const now = moment().tz(timezone);
+        const scheduled = now.startOf('day').add(daysLeft, 'days');
+        console.log('Reminder schedule: ' + scheduled.format('YYYY-MM-DDTHH:mm:00.000'));
+
+        return {
+            requestTime: now.format('YYYY-MM-DDTHH:mm:00.000'),
+            trigger: {
+                type: 'SCHEDULED_ABSOLUTE',
+                scheduledTime: scheduled.format('YYYY-MM-DDTHH:mm:00.000'),
+                timeZoneId: timezone,
+            },
+            alertInfo: {
+              spokenInfo: {
+                content: [{
+                  locale: locale,
+                  text: message,
+                }],
+              },
+            },
+            pushNotification: {
+              status: 'ENABLED',
+            }
+          }
+    },
+    
+    //obtener los datos desde la API
+    //limit es el número de respuestas que queremos
+    fetchBirthdaysData(day, month, limit){
+        //a qué servicio nos vamos a conectar
+        const endpoint = 'https://query.wikidata.org/sparql';
+        // List of actors with pictures and date of birth
+        const sparqlQuery =
+        `SELECT ?human ?humanLabel ?picture ?date_of_birth WHERE {
+        ?human wdt:P31 wd:Q5.
+        ?human wdt:P106 wd:Q33999.
+        ?human wdt:P18 ?picture.
+        FILTER((DATATYPE(?date_of_birth)) = xsd:dateTime)
+        FILTER((MONTH(?date_of_birth)) = ${month})
+        FILTER((DAY(?date_of_birth)) = ${day})
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        OPTIONAL { ?human wdt:P569 ?date_of_birth. }
+        }
+        LIMIT ${limit}`;
+        const url = endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+        const headers = {'Accept': 'application/sparql-results+json'};
+        console.log(url);
+
+        async function getJsonResponse(url, headers){
+            const res = await fetch(url, {headers});
+            return await res.json();
+        }
+
+        return getJsonResponse(url, headers).then((result) => {
+            return result;
+        }).catch((error) => {
+            return null;
+        });
+    },
+    
+    //directiva para crear la respuesta progresiva
+    callDirectiveService(handlerInput, msg) {
+        // Call Alexa Directive Service.
+        const {requestEnvelope} = handlerInput;
+        const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient();
+        const requestId = requestEnvelope.request.requestId;
+        const {apiEndpoint, apiAccessToken} = requestEnvelope.context.System;
+
+        // build the progressive response directive
+        const directive = {
+          header: {
+            requestId,
+          },
+          directive:{
+              type: 'VoicePlayer.Speak',
+              speech: msg
+          },
+        };
+        // send directive
+        return directiveServiceClient.enqueue(directive, apiEndpoint, apiAccessToken);
+    }
+}
